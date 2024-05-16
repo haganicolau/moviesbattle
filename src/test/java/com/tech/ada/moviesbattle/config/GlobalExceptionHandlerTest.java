f3cc589f0414ab0f093370615cf4b1006a4985f2
holiveira <nicolau.oliveira@wit-software.com>
2024-05-16 20:39:41 +0100

    fixing tests

--- src/test/java/com/tech/ada/moviesbattle/config/GlobalExceptionHandlerTest.java
+++ src/test/java/com/tech/ada/moviesbattle/config/GlobalExceptionHandlerTest.java
@@ -1,19 +1,12 @@
 package com.tech.ada.moviesbattle.config;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 
 public class GlobalExceptionHandlerTest {
 
--- src/test/java/com/tech/ada/moviesbattle/repository/PlayerRepositoryTest.java
+++ src/test/java/com/tech/ada/moviesbattle/repository/PlayerRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.tech.ada.moviesbattle.repository;
 
 import com.tech.ada.moviesbattle.model.Player;
-import org.hibernate.validator.internal.constraintvalidators.bv.AssertTrueValidator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
